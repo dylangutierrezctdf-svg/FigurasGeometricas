dc6b788973d3af7998d93afc3ce1b67cfe5f14ea
dylan <dylangutierrezctdf@gmail.com>
2026-06-12 15:35:33 -0600

    Se crearon las clases
    Se crean las diferentes clases de triangulo.

--- src/triangulo/Triangulo.java
+++ src/triangulo/Triangulo.java
@@ -4,10 +4,49 @@
  */
 package triangulo;
 
+import FigurasGeometricas.FigGeo;
+
 /**
  *
  * @author Student
  */
-public class Triangulo {
+public class Triangulo extends FigGeo{
+    private double ladoA;
+    private double ladoB;
+    private double ladoC;
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public double getLadoC() {
+        return ladoC;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public void setLadoC(double ladoC) {
+        this.ladoC = ladoC;
+    }
+    public Triangulo(double ladoA, double ladoB ,double ladoC){
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+    }
+    
+    @Override
+    protected double calcularPerimetro(){
+        return ladoA+ladoB+ladoC;
+    }
     
 }
